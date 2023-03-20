4a0383b286e23bf1a3732b1f600ebe1189f956ca
jm-roman <johnmroman33@gmail.com>
2023-03-20 17:17:35 -0400

    initial backend, server, db, and questions model

---
+++ server/server.ts
@@ -0,0 +1,51 @@
+import express, {
+  Application,
+  Request,
+  Response,
+  NextFunction,
+  Router,
+} from 'express';
+import cors from 'cors';
+import path from 'path';
+
+const router: Router = express.Router();
+const app: Application = express();
+const PORT: number = 8080;
+app.use(express.json());
+app.use(cors());
+
+//Define the error object type to use
+type ServerError = {
+  log: string;
+  status: number;
+  message: { err: string };
+};
+
+/**
+ * express error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+
+// global err handler
+app.use(
+  (
+    err: ServerError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response => {
+    const defaultErr: ServerError = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = { defaultErr, ...err };
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  }
+);
+
+//listening to server connection
+app.listen(PORT, () => console.log(`Server is connected on ${PORT}`));
+
+export default app;
