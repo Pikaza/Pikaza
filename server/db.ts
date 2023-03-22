1e1c452702daef8670fabc6a0b6bc4eb8e766f1f
jm-roman <johnmroman33@gmail.com>
2023-03-22 11:34:13 -0400

    new db routes for most recent

--- server/db.ts
+++ server/db.ts
@@ -4,18 +4,16 @@
  * @description Database file that will connect to the localhost postgreSQL
  * ************************************
 //  */
-import { Sequelize, DataTypes } from "sequelize";
-import * as dotenv from "dotenv";
+import { Sequelize, DataTypes } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 // import { test } from './test.js';
 // import Questions from './models/questions.model.js';
 
-const PG_URI: string = "postgres://ksfjiqbz:Gg4_sm2S12I1MS0FCB3QW-ESIPFAQzoB@ruby.db.elephantsql.com/ksfjiqbz";
+const banana = 'postgres://ksfjiqbz:Gg4_sm2S12I1MS0FCB3QW-ESIPFAQzoB@ruby.db.elephantsql.com/ksfjiqbz'
 
-const sequelize = new Sequelize(PG_URI, { logging: false });
 
-sequelize.sync({}).then(() => {
-  console.log("All models were synchronized successfully.");
-});
+const sequelize = new Sequelize(banana, { logging: false });
 
 const db: any = {
   sequelize,
@@ -25,6 +23,7 @@ const db: any = {
 // exported for use on the frontend
 export interface QuestionsAttributes {
   _id: number;
+  most_recent: string;
   question_body: string;
   frequency: number;
   company: string[];
@@ -32,7 +31,7 @@ export interface QuestionsAttributes {
   tags: string[];
 }
 
-const questions = db.sequelize.define("questions", {
+const questions = db.sequelize.define('questions', {
   // Model attributes are defined here
   _id: {
     type: DataTypes.INTEGER,
@@ -44,6 +43,11 @@ const questions = db.sequelize.define("questions", {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
+  },
+  most_recent: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+    unique: false,
   },
   frequency: {
     type: DataTypes.INTEGER,
@@ -63,7 +67,7 @@ const questions = db.sequelize.define("questions", {
     validate: {
       isSpecificLength(value: []) {
         if (value.length > 3) {
-          throw new Error("industryOfFocus must only have three items");
+          throw new Error('industryOfFocus must only have three items');
         }
       },
     },
@@ -75,7 +79,7 @@ const questions = db.sequelize.define("questions", {
     validate: {
       isSpecificLength(value: []) {
         if (value.length > 5) {
-          throw new Error("industryOfFocus must only have three items");
+          throw new Error('industryOfFocus must only have three items');
         }
       },
     },
@@ -87,11 +91,18 @@ db.questions = questions;
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.');
   })
   .catch((err) => {
-    console.log("error from db.js: ", err);
-    console.log("i did not connect");
+    console.log('error from db.js: ', err);
+    console.log('i did not connect');
   });
 
+sequelize
+  .sync({
+    /*force: true */
+  })
+  .then(() => {
+    console.log('All models were synchronized successfully.');
+  });
 export { db };
